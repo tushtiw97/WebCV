83e86257f21c5e58a9fb4e825b6be3a5ac0a46fa
tushtiw97 <tiwaritusharudayan@gmail.com>
2021-09-28 22:57:10 +0530

    Adding contact details

--- web-cv/src/app/contact/contact.component.ts
+++ web-cv/src/app/contact/contact.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { updateCurrentRouterLink } from '../route-store/route.action';
 import { CurrentRouterLink } from '../route-store/route.reducer';
+import { UtilityFunctionsService } from '../utility-functions.service';
 
 @Component({
   selector: 'app-contact',
@@ -10,12 +11,23 @@ import { CurrentRouterLink } from '../route-store/route.reducer';
 })
 export class ContactComponent implements OnInit {
 
+  componentInitialized: boolean = false;
+  applyTransition: boolean = false;
+  componentHeightCssString: string;
+
   constructor(
-    private store: Store<{routerStore: CurrentRouterLink}>
-  ) { }
+    private store: Store<{routerStore: CurrentRouterLink}>,
+    private utilityFunctionService: UtilityFunctionsService
+  ) {
+    this.componentHeightCssString = this.utilityFunctionService.computeScreenHeight();
+  }
 
   ngOnInit() {
     this.store.dispatch(updateCurrentRouterLink({currentRouterLink: window.location.pathname}));
+    this.componentInitialized = true;
+    setTimeout(() => {
+      this.applyTransition = true;
+    }, 100);
   }
 
 }
--- web-cv/src/app/models/data-for-export.ts
+++ web-cv/src/app/models/data-for-export.ts
@@ -32,133 +32,29 @@ export const userActivityTimeline: TimelineData[] = [
 
 export const projectsAndCertifications: ProjectsAndCertsData[] = [
     {
-        name: 'Project 1',
-        startYear: '2019',
-        endYear: '2019',
-        technologies: 'MEAN stack',
-        programmingLanguages: 'JavaScript',
-        repo: 'https://google.com',
-        description: 'This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description.'
+        name: 'Remote Controller',
+        startYear: 'Oct, 2019',
+        endYear: 'Jan, 2020',
+        technologies: 'Spring Framework, Angular 8, Java Robot API',
+        programmingLanguages: 'Java, JavaScript, TypeScript',
+        repo: 'https://github.com/tushtiw97/remote-controller-ui',
+        description: 'A simple remote controller with an executable service and a mobile control that allows us to control streaming services throughout different platforms (like Netflix, Primevideo, Hotstar, etc.) through a tailor made remote controller'
     }, {
-        name: 'Project 1',
-        startYear: '2019',
-        endYear: '2019',
-        technologies: 'MEAN stack',
-        programmingLanguages: 'JavaScript',
-        repo: 'https://google.com',
-        description: 'This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description.'
+        name: 'Kafka Trade Arbitrage',
+        startYear: 'Jan, 2019',
+        endYear: 'March, 2019',
+        technologies: 'Spring Framework, Kafka Publish/Subscribe Message Broker, Yahoo Finance API (free subscription)',
+        programmingLanguages: 'Java',
+        repo: 'https://github.com/tushtiw97/KafkaTradeArbitrage',
+        description: 'An application that allows users to leverage the minor glitches in stock markets (arbitrage) wherein the price of a stock in one exchange is slightly more or less compared to equivalent stock in a different exchange, thus allowing users to make small, but certain profits'
     }, {
-        name: 'Project 1',
-        startYear: '2019',
-        endYear: '2019',
-        technologies: 'MEAN stack',
-        programmingLanguages: 'JavaScript',
-        repo: 'https://google.com',
-        description: 'This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description.'
-    }, {
-        name: 'Project 1',
-        startYear: '2019',
-        endYear: '2019',
-        technologies: 'MEAN stack',
-        programmingLanguages: 'JavaScript',
-        repo: 'https://google.com',
-        description: 'This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description.'
-    }, {
-        name: 'Project 1',
-        startYear: '2019',
-        endYear: '2019',
-        technologies: 'MEAN stack',
-        programmingLanguages: 'JavaScript',
-        repo: 'https://google.com',
-        description: 'This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description.'
-    }, {
-        name: 'Project 1',
-        startYear: '2019',
-        endYear: '2019',
-        technologies: 'MEAN stack',
-        programmingLanguages: 'JavaScript',
-        repo: 'https://google.com',
-        description: 'This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description.'
-    }, {
-        name: 'Project 1',
-        startYear: '2019',
-        endYear: '2019',
-        technologies: 'MEAN stack',
-        programmingLanguages: 'JavaScript',
-        repo: 'https://google.com',
-        description: 'This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description.'
-    }, {
-        name: 'Project 1',
-        startYear: '2019',
-        endYear: '2019',
-        technologies: 'MEAN stack',
-        programmingLanguages: 'JavaScript',
-        repo: 'https://google.com',
-        description: 'This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description.'
-    }, {
-        name: 'Project 1',
-        startYear: '2019',
-        endYear: '2019',
-        technologies: 'MEAN stack',
-        programmingLanguages: 'JavaScript',
-        repo: 'https://google.com',
-        description: 'This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description.'
-    }, {
-        name: 'Project 1',
-        startYear: '2019',
-        endYear: '2019',
-        technologies: 'MEAN stack',
-        programmingLanguages: 'JavaScript',
-        repo: 'https://google.com',
-        description: 'This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description.'
-    }, {
-        name: 'Project 1',
-        startYear: '2019',
-        endYear: '2019',
-        technologies: 'MEAN stack',
-        programmingLanguages: 'JavaScript',
-        repo: 'https://google.com',
-        description: 'This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description.'
-    }, {
-        name: 'Project 1',
-        startYear: '2019',
-        endYear: '2019',
-        technologies: 'MEAN stack',
-        programmingLanguages: 'JavaScript',
-        repo: 'https://google.com',
-        description: 'This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description.'
-    }, {
-        name: 'Project 1',
-        startYear: '2019',
-        endYear: '2019',
-        technologies: 'MEAN stack',
-        programmingLanguages: 'JavaScript',
-        repo: 'https://google.com',
-        description: 'This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description.'
-    }, {
-        name: 'Project 1',
-        startYear: '2019',
-        endYear: '2019',
-        technologies: 'MEAN stack',
-        programmingLanguages: 'JavaScript',
-        repo: 'https://google.com',
-        description: 'This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description.'
-    }, {
-        name: 'Project 1',
-        startYear: '2019',
-        endYear: '2019',
-        technologies: 'MEAN stack',
-        programmingLanguages: 'JavaScript',
-        repo: 'https://google.com',
-        description: 'This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description.'
-    }, {
-        name: 'Project 1',
-        startYear: '2019',
-        endYear: '2019',
-        technologies: 'MEAN stack',
-        programmingLanguages: 'JavaScript',
-        repo: 'https://google.com',
-        description: 'This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description. This is the project description.'
+        name: 'Seat Booking System',
+        startYear: 'July, 2018',
+        endYear: 'Dec, 2018',
+        technologies: 'Laravel Framework',
+        programmingLanguages: 'JavaScript, PHP',
+        repo: 'https://github.com/tushtiw97/Seat_Matrix',
+        description: 'A working prototype of a real-time seat booking system which allows users to book seats for movies, events, screenings, etc., while maintaining live monitoring of booked/occupied seats, and a notification system to notify users in case of successful bookings, or failures'
     }
 ];
 
